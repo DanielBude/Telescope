45aafb86145973de43a1488cf6192b58a423be01
DanielBude <danielbude@gmx.de>
2016-02-22 20:57:30 +0100

    htgfcdxs

--- Telescope/src/app/Main.java
+++ Telescope/src/app/Main.java
@@ -1,5 +1,16 @@
 package app;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
 
+public class Main
+{
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(new JLabel("Hello, world!"));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(100, 100);
+		frame.setVisible(true);
+	}
 }
