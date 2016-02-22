b186539dfe65eab0c19b57b2d79b064952742f4d
MichaelWeis <m.weis@gmx.net>
2016-02-22 22:31:37 +0100

    main method deleted

--- Telescope/src/com/TelescopeDesign/app/Calculation.java
+++ Telescope/src/com/TelescopeDesign/app/Calculation.java
@@ -2,12 +2,6 @@ package com.TelescopeDesign.app;
 
 public class Calculation {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
 
 }
