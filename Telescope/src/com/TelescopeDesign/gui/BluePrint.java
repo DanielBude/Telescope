c065910878aecc3cf82146e3db7674d4e34fe77b
DanielBude <danielbude@gmx.de>
2016-02-29 22:10:02 +0100

    Probieren etwas zu zeichnen

--- Telescope/src/com/TelescopeDesign/gui/BluePrint.java
+++ Telescope/src/com/TelescopeDesign/gui/BluePrint.java
@@ -1,6 +1,7 @@
 package com.TelescopeDesign.gui;
 
 import java.awt.Color;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
@@ -8,11 +9,62 @@ import javax.swing.JPanel;
 
 public class BluePrint extends JPanel{
 
+	// Hier sollten die Entsprechenden Objekte gespeichert sein
+	
+	
 	public BluePrint()
 	{
-		
+		repaint();
 		//Define Backgroudcolor
-		setBackground(Color.BLUE);
+		//setBackground(Color.BLUE);
+	}
+	
+	public void paintComponent(Graphics g){
+		//Überschreiben des alten Bildes
+		super.paintComponent(g);
+		
+		//Viereck zeichnen
+		g.drawRect(20, 20, 50, 50);
+		
+		//Ausgefülltes Viereck zeichnen
+		g.setColor(Color.RED);
+		g.fillRect(20, 80, 50, 80);
+		
+		//Zeichnen eines Polygons
+		int[] xCoords = {120,145,170};
+		int[] yCoords = {70,20,70};
+		g.fillPolygon(
+			xCoords, 
+			yCoords, 
+			xCoords.length);
+		
+		//Rand des Polygons
+		g.setColor(Color.BLACK);
+		g.drawPolygon(
+			xCoords, 
+			yCoords, 
+			xCoords.length);
+		
+		//Kreis- bzw. Kreisbogen zeichnen
+		g.setColor(
+			new Color(255, 255, 0));
+		g.fillArc(
+			220, 20, 100, 100, 45, 200);
+		
+		//Rand des Kreises zeichnen
+		g.setColor(
+			Color.BLACK);
+		g.drawArc(
+			220, 20, 100, 100, 0, 360);
+		
+		//Oval zeichnen
+		g.drawOval(
+			370, 20, 100, 150);
+		
+		g.drawArc(300, 300, 15, 150, -90, 180);
+		g.drawLine(307, 300, 320, 300);
+		g.drawLine(307, 450, 320, 450);
+		g.drawLine(320, 300, 320, 450);
 	}
 	
 }
--- Telescope/src/com/TelescopeDesign/gui/MainFrame.java
+++ Telescope/src/com/TelescopeDesign/gui/MainFrame.java
@@ -1,17 +1,20 @@
 package com.TelescopeDesign.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.*;
 
 
 public class MainFrame extends JFrame
 {
+	private BluePrint bp = new BluePrint();
 		/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-		public MainFrame()
+	public MainFrame()
 	{
 		this.getContentPane().add(new JLabel("Hello, world!"));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -47,7 +50,7 @@ public class MainFrame extends JFrame
 		
 		JTabbedPane tab = new JTabbedPane();
 		tab.setTabPlacement(JTabbedPane.BOTTOM);
-		tab.add("Blue Print", new BluePrint());
+		tab.add("Blue Print", bp);
 		tab.add("Raytracing",new Raytracing());
 		getContentPane().add(tab, BorderLayout.CENTER);
 		
@@ -58,12 +61,15 @@ public class MainFrame extends JFrame
 		placeHolder.setSize(200, 100);
 		getContentPane().add(placeHolder, BorderLayout.SOUTH);		
 		
-		
-	
-		
 		this.setSize(getMaximumSize());
+		this.setMinimumSize(new Dimension(500,300));
 		this.setVisible(true);
 		
+		// Scrollbar sollte noch hinzugefügt werden
 	}
-	
+
+	public BluePrint getBluePrint()
+	{
+		return bp;
+	}
 }
