144345f30a52e425aadb6874eff1cd6be8888977
MichaelWeis <clearskies@gmx.net>
2016-03-14 21:12:21 +0100

    extends from DataObject

--- Telescope/src/com/TelescopeDesign/datamodel/PrimaryMirror.java
+++ Telescope/src/com/TelescopeDesign/datamodel/PrimaryMirror.java
@@ -1,19 +1,39 @@
 package com.TelescopeDesign.datamodel;
 
-public class PrimaryMirror {
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class PrimaryMirror extends DataObject{
 	
 	private double _diameter;
 	private double _radius;
+	private double _thickness;
+	private double _focalLength;
 	private double _x;
 	private double _y;
+	
+    private ArrayList<String> _header;
+	private Hashtable<String, Double> _properties;
 	
 	
 	public PrimaryMirror()
 	{
 		_diameter = 6;
 		_radius = 10;
+		_thickness = 20;
+		_focalLength = 0.0;
 		_x = 200;
 		_y = 100;
+		
+		_header = new ArrayList<String>();
+		_header.add("Primary Mirror");
+		_header.add("");
+		
+		_properties = new Hashtable<String, Double>();				
+		_properties.put("Diameter [mm]", _diameter);
+		_properties.put("Radius [mm]", _radius);
+		_properties.put("Thickness [mm]", _thickness);
+		_properties.put("Focal lenght [mm]", _focalLength);
 	}
 
 
@@ -54,6 +74,17 @@ public class PrimaryMirror {
 
 	public void set_y(double _y) {
 		this._y = _y;
+	}
+
+
+	@Override
+	public Hashtable<String, Double> getData() {		
+		return _properties;
+	}
+
+	@Override
+	public ArrayList<String> getHeader() {
+		return _header;
 	}
 
 	
