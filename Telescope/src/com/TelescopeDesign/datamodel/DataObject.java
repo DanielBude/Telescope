38ea80b259def90787ae733b779e3341fcb5cf4e
MichaelWeis <clearskies@gmx.net>
2016-04-08 21:38:02 +0200

    Deleted DataObject

--- Telescope/src/com/TelescopeDesign/datamodel/DataObject.java
+++
@@ -1,9 +0,0 @@
-package com.TelescopeDesign.datamodel;
-
-import java.util.ArrayList;
-import java.util.Hashtable;
-
- public abstract class DataObject{
- public abstract Hashtable<String,Double> getData();
- public abstract ArrayList<String> getHeader();
-}
